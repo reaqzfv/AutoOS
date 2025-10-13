bb2bff8f15da729c4a861f4e9eca0ce64e50ee1c
tinodin <constantin.schlueter@icloud.com>
2025-10-13 19:02:37 +0200

    Improve TimeDateRegionStage

--- Views/Installer/Stages/TimeDateRegionStage.cs
+++ Views/Installer/Stages/TimeDateRegionStage.cs
@@ -24,14 +24,12 @@ public static class TimeDateRegionStage
 
         var actions = new List<(string Title, Func<Task> Action, Func<bool> Condition)>
         {
-            // set time zone automatically
-            ($"Setting time zone to {GetWindowsTimeZone(countryCode)}", async () => await ProcessActions.RunNsudo("CurrentUser", $@"powershell -Command ""Set-TimeZone -Id '{GetWindowsTimeZone(countryCode)}'"""), null),
+            // set time zone
+            ($"Setting time zone to {GetWindowsTimeZone(countryCode)}", async () => await ProcessActions.RunPowerShell($@"Set-TimeZone -Id '{GetWindowsTimeZone(countryCode)}'"), null),
+            ($"Setting time zone to {GetWindowsTimeZone(countryCode)}", async () => await ProcessActions.Sleep(1000), null),
 
-            // set keyboard layout automatically
-            ($"Setting keyboard layout to {GetKeyboardLayout(countryCode)}", async () => await ProcessActions.RunNsudo("CurrentUser", $@"powershell -Command ""$langList = New-WinUserLanguageList en-US; $langList[0].InputMethodTips.Clear(); $langList[0].InputMethodTips.Add('{GetKeyboardLayout(countryCode)}'); Set-WinUserLanguageList $langList -Force"""), null),
-
-            // set regional format automatically
-            ($"Setting regional format to en-{countryCode}", async () => await ProcessActions.RunNsudo("CurrentUser", $@"powershell -Command ""Set-Culture en-{countryCode}"""), null),
+            // set keyboard layout
+            ($"Setting keyboard layout to en-{countryCode}", async () => await ProcessActions.RunPowerShell($@"$langList=Get-WinUserLanguageList;$langList=$langList|Where-Object {{$_.LanguageTag -ne 'en-US'}};$newLang=New-WinUserLanguageList 'en-{countryCode}';$langList+=$newLang[0];Set-WinUserLanguageList $langList -Force"), null),
 
             // sync time
             ("Syncing time", async () => await ProcessActions.RunNsudo("CurrentUser", "net start w32time"), null),
@@ -39,7 +37,7 @@ public static class TimeDateRegionStage
             ("Syncing time", async () => await ProcessActions.RunNsudo("CurrentUser", "net stop w32time"), null),
 
             // apply changes to the whole system
-            ("Applying changes to the whole system", async () => await ProcessActions.RunNsudo("CurrentUser", @"powershell -Command ""Copy-UserInternationalSettingsToSystem -WelcomeScreen $true -NewUser $true"""), null),
+            ("Applying changes to the whole system", async () => await ProcessActions.RunPowerShell(@"Copy-UserInternationalSettingsToSystem -WelcomeScreen $true -NewUser $true"), null),
         };
 
         var filteredActions = actions.Where(a => a.Condition == null || a.Condition.Invoke()).ToList();
@@ -329,118 +327,6 @@ public static class TimeDateRegionStage
             "ZM" => "Central Africa Time",
             "ZW" => "Central Africa Time",
             _ => "UTC"
-        };
-    }
-
-    private static string GetKeyboardLayout(string countryCode)
-    {
-        return countryCode switch
-        {
-            "AF" => "0409:00000401", // Arabic (Saudi Arabia)
-            "AM" => "0409:0000045E", // Amharic
-            "AR" => "0409:0000040A", // Spanish (Argentina)
-            "AS" => "0409:0000044D", // Assamese
-            "AT" => "0409:00000407", // German (Austria)
-            "AU" => "0409:00000C09", // English (Australia)
-            "AZ" => "0409:0000042C", // Azerbaijani (Latin)
-            "BE" => "0409:00000813", // Dutch (Belgium)
-            "BG" => "0409:00000402", // Bulgarian
-            "BN" => "0409:00000445", // Bangla (Bangladesh)
-            "BO" => "0409:0000040A", // Spanish (Bolivia)
-            "BR" => "0409:00000416", // Portuguese (Brazil)
-            "BS" => "0409:0000201A", // Bosnian (Latin)
-            "BY" => "0409:00000423", // Belarusian
-            "CA" => "0409:00001009", // English (Canada)
-            "CH" => "0409:00001007", // German (Switzerland)
-            "CL" => "0409:0000040A", // Spanish (Chile)
-            "CN" => "0409:00000804", // Chinese (Simplified)
-            "CO" => "0409:0000040A", // Spanish (Colombia)
-            "CR" => "0409:0000040A", // Spanish (Costa Rica)
-            "CS" => "0409:00000405", // Czech
-            "CY" => "0409:00000452", // Welsh
-            "CZ" => "0409:00000405", // Czech
-            "DA" => "0409:00000406", // Danish
-            "DE" => "0409:00000407", // German (Germany)
-            "DK" => "0409:00000406", // Danish
-            "DO" => "0409:0000040A", // Spanish (Dominican Republic)
-            "EC" => "0409:0000040A", // Spanish (Ecuador)
-            "EE" => "0409:00000425", // Estonian
-            "EG" => "0409:00000401", // Arabic (Egypt)
-            "ES" => "0409:0000040A", // Spanish (Spain)
-            "ET" => "0409:00000425", // Estonian
-            "FI" => "0409:0000040B", // Finnish
-            "FR" => "0409:0000040C", // French (France)
-            "GA" => "0409:00001809", // Irish
-            "GB" => "0409:00000809", // English (United Kingdom)
-            "GR" => "0409:00000408", // Greek
-            "GT" => "0409:0000040A", // Spanish (Guatemala)
-            "HK" => "0409:00000C04", // Chinese (Traditional, Hong Kong)
-            "HN" => "0409:0000040A", // Spanish (Honduras)
-            "HR" => "0409:0000041A", // Croatian
-            "HU" => "0409:0000040E", // Hungarian
-            "ID" => "0409:00000421", // Indonesian
-            "IE" => "0409:00001809", // Irish
-            "IL" => "0409:0000040D", // Hebrew
-            "IN" => "0409:00000439", // Hindi
-            "IQ" => "0409:00000401", // Arabic (Iraq)
-            "IS" => "0409:0000040F", // Icelandic
-            "IT" => "0409:00000410", // Italian (Italy)
-            "JM" => "0409:00000409", // English (Jamaica)
-            "JO" => "0409:00000401", // Arabic (Jordan)
-            "JP" => "0409:00000411", // Japanese
-            "KE" => "0409:00000409", // English (Kenya)
-            "KG" => "0409:00000440", // Kyrgyz (Cyrillic)
-            "KH" => "0409:00000453", // Khmer
-            "KR" => "0409:00000412", // Korean
-            "KW" => "0409:00000401", // Arabic (Kuwait)
-            "KZ" => "0409:0000043F", // Kazakh
-            "LA" => "0409:00000454", // Lao
-            "LB" => "0409:00000401", // Arabic (Lebanon)
-            "LT" => "0409:00000427", // Lithuanian
-            "LU" => "0409:0000040C", // French (Luxembourg)
-            "LV" => "0409:00000426", // Latvian
-            "LY" => "0409:00000401", // Arabic (Libya)
-            "MA" => "0409:00000401", // Arabic (Morocco)
-            "MK" => "0409:0000042F", // Macedonian
-            "MN" => "0409:00000450", // Mongolian (Cyrillic)
-            "MO" => "0409:00001404", // Chinese (Traditional, Macao)
-            "MX" => "0409:0000080A", // Spanish (Mexico)
-            "MY" => "0409:0000043E", // Malay (Malaysia)
-            "NG" => "0409:00000409", // English (Nigeria)
-            "NL" => "0409:00000413", // Dutch (Netherlands)
-            "NO" => "0409:00000414", // Norwegian (Bokmål)
-            "NZ" => "0409:00000409", // English (New Zealand)
-            "OM" => "0409:00000401", // Arabic (Oman)
-            "PA" => "0409:0000040A", // Spanish (Panama)
-            "PE" => "0409:0000040A", // Spanish (Peru)
-            "PH" => "0409:0000042D", // Filipino
-            "PK" => "0409:00000420", // Urdu
-            "PL" => "0409:00000415", // Polish
-            "PT" => "0409:00000816", // Portuguese (Portugal)
-            "PY" => "0409:0000040A", // Spanish (Paraguay)
-            "QA" => "0409:00000401", // Arabic (Qatar)
-            "RO" => "0409:00000418", // Romanian (Romania)
-            "RS" => "0409:00000C1A", // Serbian (Latin, Serbia)
-            "RU" => "0409:00000419", // Russian
-            "SA" => "0409:00000401", // Arabic (Saudi Arabia)
-            "SE" => "0409:0000041D", // Swedish
-            "SG" => "0409:00000409", // English (Singapore)
-            "SI" => "0409:00000424", // Slovenian
-            "SK" => "0409:0000041B", // Slovak
-            "SV" => "0409:0000040A", // Spanish (El Salvador)
-            "TH" => "0409:0000041E", // Thai
-            "TN" => "0409:00000401", // Arabic (Tunisia)
-            "TR" => "0409:0000041F", // Turkish
-            "UA" => "0409:00000422", // Ukrainian
-            "US" => "0409:00000409", // English (United States)
-            "UY" => "0409:0000040A", // Spanish (Uruguay)
-            "UZ" => "0409:00000443", // Uzbek (Latin)
-            "VE" => "0409:0000040A", // Spanish (Venezuela)
-            "VN" => "0409:0000042A", // Vietnamese
-            "YE" => "0409:00000401", // Arabic (Yemen)
-            "ZA" => "0409:00000409", // English (South Africa)
-            "ZW" => "0409:00000409", // English (Zimbabwe)
-            _ => null
         };
     }
 }
